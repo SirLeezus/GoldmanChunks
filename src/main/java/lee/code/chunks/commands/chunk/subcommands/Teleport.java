a5d32f3d10656a1d6e33d124d037c8a0b0785edf
Lee <leecook808@gmail.com>
2021-06-24 21:24:57 -0700

    Teleport update

---
+++ src/main/java/lee/code/chunks/commands/chunk/subcommands/Teleport.java
@@ -0,0 +1,57 @@
+package lee.code.chunks.commands.chunk.subcommands;
+
+import lee.code.chunks.GoldmanChunks;
+import lee.code.chunks.commands.SubCommand;
+import lee.code.chunks.database.Cache;
+import lee.code.chunks.lists.Lang;
+import org.bukkit.Location;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.UUID;
+
+public class Teleport extends SubCommand {
+
+    @Override
+    public String getName() {
+        return "teleport";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Teleport to a chunk you own.";
+    }
+
+    @Override
+    public String getSyntax() {
+        return "/teleport &f<chunk>";
+    }
+
+    @Override
+    public String getPermission() {
+        return "chunk.command.teleport";
+    }
+
+    @Override
+    public void perform(Player player, String[] args) {
+        GoldmanChunks plugin = GoldmanChunks.getPlugin();
+        Cache cache = plugin.getCache();
+
+        if (args.length > 1) {
+            UUID uuid = player.getUniqueId();
+            String chunk = args[1].trim();
+            System.out.println(chunk);
+            if (cache.isChunkClaimed(chunk)) {
+                if (cache.isChunkOwner(chunk, uuid)) {
+                    Location chunkLocation = plugin.getPU().unFormatChunkLocation(chunk);
+                    plugin.getPU().teleportPlayerToChunk(player, chunkLocation);
+                } else player.sendMessage("Not owner.");
+            } else player.sendMessage("Not claimed.");
+        }
+    }
+
+    @Override
+    public void performConsole(CommandSender console, String[] args) {
+        console.sendMessage(Lang.PREFIX.getComponent(null).append(Lang.ERROR_NOT_A_CONSOLE_COMMAND.getComponent(null)));
+    }
+}
